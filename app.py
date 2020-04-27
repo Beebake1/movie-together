a0dc82d5b30d8269ee77b92834d6b1f4bc7903cc
Beebake1 <bibekkhatri81@gmail.com>
2020-04-27 12:49:13 +0545

    no message

--- app.py
+++ app.py
@@ -104,8 +104,8 @@ def embeded(movie,room,username):
             js = js.replace('[[room]]',room)
             iframe_inner = iframe_inner.replace('</html>','<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.4.8/socket.io.min.js"></script><script>'+js+'</script>')
             content = Markup(iframe_inner)
-        
-    content = Markup('<h1>Movie not Found</h1>')
+        else:
+            content = Markup('<h1>Movie not Found</h1>')
     return render_template('watch/embeded/movie_embeded.html',content=content)
 
 if __name__=='__main__':
