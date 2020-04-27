803801a0930e0a70915f7cc71bef4379791adfe5
Beebake1 <bibekkhatri81@gmail.com>
2020-04-27 12:45:00 +0545

    no message

--- app.py
+++ app.py
@@ -80,30 +80,13 @@ def movie(movie_name):
 
 @app.route('/watch/<string:room>/<string:movie>/')
 def room(room,movie):
-    
-    for rim in movie_rooms:
-        if(rim['room'] == (room)):
-            username = get_random(5)
-            data = {
-                'username' : username,
-                 'room'  : room,
-                 'movie' : rim['movie']
-        }
-    response=fetch(url+'movie/'+ movie)
-    movie_soup = BeautifulSoup(response.text,'html.parser')
-    movie_servers = movie_soup.findAll('li',attrs={'class':'mb5'})
-    content =''
-    for server in movie_servers:
-        iframe_source = fetch(url+'embed-src/?url='+server.find('a')['data-id']+'&t=0')
-        iframe_text = iframe_source.text
-        if(iframe_text.find('Clappr') != -1):
-            iframe_inner = iframe_text
-            js = read('static/js/player.js')
-            js = js.replace('[[username]]',username)
-            js = js.replace('[[room]]',room)
-            iframe_inner = iframe_inner.replace('</html>','<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.4.8/socket.io.min.js"></script><script>'+js+'</script>')
-            content = Markup(iframe_inner)
-    return render_template('watch/embeded/movie_embeded.html',content=content)
+   
+    data = {
+            'username' : get_random(5),
+                'room'  : room,
+                'movie' : movie
+    }
+    return render_template('watch/movie.html',data=data)
 
 @app.route('/embeded/<string:movie>/<string:room>/<string:username>/')
 def embeded(movie,room,username):
