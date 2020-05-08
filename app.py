289735ddd3daa213cb3129606519041159d2468f
Beebake1 <bibekkhatri81@gmail.com>
2020-05-08 21:05:28 +0545

    no message

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 import random
 import string
 
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask,render_template,request,redirect,Markup
 from flask_socketio import SocketIO, send,join_room,leave_room,emit,rooms
 from bs4 import BeautifulSoup
@@ -14,8 +15,25 @@ from utils.file_helper import read
 app = Flask(__name__,static_url_path='/static')
 app.config['SECRET_KEY'] = os.environ.get('SECRET')
 
-socketio = SocketIO(app)
+eng_url = "https://bmoviesfree.page/"
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://tbcptjvkoiotvd:95b2637180a39cd15a8755567b00631ef441a48bd7f1f491fb2a76591ed38979@ec2-54-75-246-118.eu-west-1.compute.amazonaws.com:5432/d8ntst072imlh7'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+socketio = SocketIO(app)
+movie_rooms = []
+
+class Bmovies(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(220),unique=True)
+    image = db.Column(db.String(220),unique=True)
+    link = db.Column(db.String(220),unique=True)
+
+    def __init__(self, title,image,link):
+        self.title = title
+        self.image = image
+        self.link = link
 @socketio.on('join_room',namespace='/event')
 def handle_join_room_event(data):
     join_room(data['room'])
@@ -36,11 +54,27 @@ def handle_send_message_event(data):
 @socketio.on('join_chat_room',namespace='/message')
 def handle_join_chat_room_event(data):
     join_room(data['room'])
+    room_data =''
+    for movie_room in movie_rooms:
+        if(movie_room['room']==data['room']):
+            room_users = movie_room['users']
+            room_users.append(data['username'])
+            movie_room.update(users = room_users)
+            room_data = movie_room
+    data.update(room_data = room_data)        
     emit('join_room_announcement',data,room=data['room'])
 
 @socketio.on('leave_chat_room',namespace='/message')
 def handle_leave_chat_room_event(data):
     leave_room(data['room'])
+    room_data =''
+    for movie_room in movie_rooms:
+        if(movie_room['room']==data['room']):
+            room_users = movie_room['users']
+            room_users.remove(data['username'])
+            movie_room.update(users = room_users)
+            room_data = movie_room
+    data.update(room_data = room_data)      
     emit('leave_room_announcement', data, room=data['room'])
 url = "https://hindimovies.to/"
 
@@ -77,6 +111,12 @@ def movie(movie_name):
     movie_rooms.append({'room':room,'movie':keyword})
     return  redirect('/watch/'+room+'/'+keyword+'/',)
 
+@app.route('/search/<string:movie_name>/')
+def movie(movie_name):
+    keyword = movie_name
+    room = get_random(12)
+    movie_rooms.append({'room':room,'movie':keyword})
+    return  redirect('/watch/'+room+'/'+keyword+'/',)
 
 @app.route('/watch/<string:room>/<string:movie>/')
 def room(room,movie):
@@ -109,5 +149,66 @@ def embeded(movie,room,username):
             content = Markup('<h1>Movie not Found</h1>')
     return render_template('watch/embeded/movie_embeded.html',content=content)
 
+@app.route('/eng/search',methods = ["POST","GET"])
+def bmovies_search():
+    if request.method == "POST":
+        keyword = request.form['search']
+        list_template = '<div class="items"><a href="[[movie_link]]"><div class="ite"><img src="[[img-src]]" class="image" alt="">[[movie_title]]</a></div></div>'
+        movies = db.session.execute("select * from public.bmovies where title ilike '%"+keyword+"%'")
+        html = ''
+        for movie in movies:
+            content = list_template.replace('[[movie_link]]',movie['link'].replace(eng_url,'/eng/'))
+            content = content.replace('[[img-src]]',movie['image'])
+            content = content.replace('[[movie_title]]',movie['title'])
+            html+=content
+        data = Markup(html)
+        return render_template('movie_list.html',data = data)
+    else:
+        return render_template('index.html')  
+
+@app.route('/eng/movie/<string:movie_name>/')
+def eng_movie(movie_name):
+    keyword = movie_name
+    room = get_random(20)
+    movie_rooms.append({'room':room,'movie':'eng/'+keyword,'users' : []})
+    return  redirect('/watch/'+room+'/',)
+
+@app.route('/embeded/eng/<string:movie>/<string:room>/<string:username>/')
+def eng_embeded(movie,room,username):
+    response=fetch(eng_url+'movie/'+ movie+'/watching/?server_id=6')
+    movie_soup = BeautifulSoup(response.text,'html.parser')
+    token = movie_soup.find('div',attrs={'id':'iframes'})['data-onlystream']
+    content =''
+    iframe_source = fetch('https://vidoo.streamango.to/e/'+token)
+    iframe_text = iframe_source.text
+    iframe_inner = iframe_text.replace('<title>','<!--<title>')
+    iframe_inner = iframe_inner.replace('</title>','</title>-->')
+    js = read('static/js/player_jw.js')
+    js = js.replace('[[username]]',username)
+    js = js.replace('[[room]]',room)
+    iframe_inner = iframe_inner.replace('</HTML>','<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.4.8/socket.io.min.js"></script><script>'+js+'</script>')
+    content = Markup(iframe_inner)
+    return render_template('watch/embeded/movie_embeded.html',content=content)  
+
+@app.route('/eng/update')
+def update_bmovies_list():
+    i = 1
+    while(True):
+        response=fetch('https://bmoviesfree.page/movies/page/'+str(i)+'/')
+        movie_soup = BeautifulSoup(response.text,'html.parser')
+        movie_list = movie_soup.findAll('div',attrs={'class':'item'})
+        if(len(movie_list) == 0):
+            break;
+        for movie in movie_list:
+            movie_link = movie.find('a',attrs={'class':'name'})['href']
+            movie_image = movie.find('img',attrs={'class':'lazy'})['src']
+            movie_name = movie.find('a',attrs={'class':'name'}).text
+            db.session.add(Bmovies(movie_name,movie_image,movie_link))
+            db.session.commit()
+        i+=1
+    data = Markup('<h1>Done</h1>')    
+    return render_template('movie_list.html',data=data)
+
 if __name__=='__main__':
+    db.create_all()
     socketio.run()
---
+++ static/js/player_jw.js
@@ -0,0 +1,80 @@
+$(function() {
+
+    const socket = io.connect('http://192.168.1.84:9000/event');
+    const username = '[[username]]';
+    const room = '[[room]]'
+
+    socket.on('receive_event', function(data) {
+        if (username != data.username) {
+            if (data.event === 'play') {
+                jwplayer().play();
+            } else if (data.event === 'pause') {
+                jwplayer().pause(true);
+            } else if (data.event === 'seek') {
+                player.seek(parseFloat(data.time));
+            }
+        }
+    });
+
+    player.on('seek', function(x) {
+        if (x.offset.toFixed(2) != player.getCurrentTime().toFixed(2)) {
+            socket.emit('event_alert', {
+                username: username,
+                room: room,
+                event: 'seek',
+                time: x.offset.toString()
+            });
+        }
+    });
+
+    player.on('play', function() {
+        socket.emit('event_alert', {
+            username: username,
+            room: room,
+            event: 'play',
+            time: player.getCurrentTime().toString()
+        });
+    });
+
+    player.on('pause', function() {
+        socket.emit('event_alert', {
+            username: username,
+            room: room,
+            event: 'pause',
+            time: player.getCurrentTime().toString()
+        });
+    });
+
+    $(document).on('click', '.pause-video', function() {
+        socket.emit('event_alert', {
+            username: username,
+            room: room,
+            event: 'pause',
+            time: '0'
+        });
+    });
+    $(document).on('click', 'btn-change', function() {
+        $('#exampleModalLong').modal('show');
+        if (username === '') {
+            console.log('blank');
+        } else {
+            $('.username').val('');
+            $('#exampleModalLong').modal('hide');
+        }
+    });
+
+    window.onbeforeunload = function() {
+        socket.emit('leave_room', {
+            username: username,
+            room: room
+        })
+    };
+
+    socket.on('connect', function() {
+        socket.emit('join_room', {
+            username: username,
+            room: room
+        });
+    });
+
+});
